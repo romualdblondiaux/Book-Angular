790c467d672be0e6afb5944b823f979e0e558440
romualdblondiaux <romuald.blondiaux@gmail.com>
2021-03-11 17:02:44 +0100

    gestion bdd book

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,49 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+import firebase from 'firebase';
+
+@Injectable()
 export class AuthService {
 
   constructor() { }
+
+  // methode pour créer un nouvel utilisateur
+  createNewUser(email: string, password: string): any {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve('done');
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  // methode pour connecter un utilsateur existant
+  signInUser(email: string, password: string): any {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve('done');
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  // methode pour se déconnecter
+  signOutUser(): void {
+    firebase.auth().signOut();
+}
+
 }
 
 
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,66 @@
 import { Injectable } from '@angular/core';
+import firebase from 'firebase';
+import { Subject } from 'rxjs/Subject';
+import { Book } from '../models/book.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class BooksService {
 
-  constructor() { }
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+
+  constructor() {
+    this.getBooks();
+  }
+
+  emitBooks(): void {
+    this.booksSubject.next(this.books);
+  }
+
+  saveBooks(): void {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks(): void {
+    firebase.database().ref('/books')
+      .on('value', (data) => {
+          this.books = data.val() ? data.val() : [];
+          this.emitBooks();
+        }
+      );
+  }
+
+  getSingleBook(id: number): any {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  createNewBook(newBook: Book): void {
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  removeBook(book: Book): void {
+    const bookIndexToRemove = this.books.findIndex(
+      (bookEl) => {
+        if (bookEl === book) {
+          return true;
+        }
+      }
+    );
+    this.books.splice(bookIndexToRemove, 1);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
 }
